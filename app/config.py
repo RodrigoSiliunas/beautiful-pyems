699cdf1098a5b85d815dabb2935c87b9fea1df45
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2022-01-22 20:52:36 -0300

    .

--- app/config.py
+++ app/config.py
@@ -16,7 +16,7 @@ class Config:
 
 class ProdConfig(Config):
     FLASK_ENV = 'production'
-    MONGO_URI = str(os.environ.get('MONGO_URI', None))
+    MONGO_URI = os.environ.get('MONGO_URI', None)
     DEBUG = False
     TESTING = False
 
